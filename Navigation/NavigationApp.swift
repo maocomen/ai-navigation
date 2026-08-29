31886df3aad0f97ff5ef70501a98b2941da7f874
马鑫 <13554778196@163.com>
2026-08-29 21:55:48 +0800

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by apple on 2026/8/29.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationTests/NavigationTests.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by apple on 2026/8/29.
+//
+
+import Testing
+@testable import Navigation
+
+struct NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
